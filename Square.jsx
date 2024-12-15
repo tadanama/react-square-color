db4546a97729f158932f52115bb07319f1fe2b91
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-16 07:36:06 +0800

    Display color state in the square

--- Square.jsx
+++ Square.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-function Square() {
+function Square({ color }) {
 	return (
 		<article>
-			<div className="square"></div>
+			<div className="square">
+				<h1>{color}</h1>
+			</div>
 		</article>
 	);
 }
